98dbab9dc65d230e20b769ef510006b2fa8e57e4
Ali <66390544+Alinoureddine1@users.noreply.github.com>
2023-01-02 09:29:06 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,11 @@
 
 #include <iostream>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <cstdlib>
 using namespace std;
 
 char grid[10] = { '0','1','2','3','4','5','6','7','8','9' };
@@ -51,13 +57,29 @@ int status() {
 
 }
 
+
+void brainplay() {
+
+	while (true) {
+		int choice = (rand() % 9) + 1;
+		if (grid[choice] == 'X' || grid[choice] == 'O')
+		{
+			continue;
+		}
+		else
+		{
+			grid[choice] = 'O';
+			break;
+		}
+	}
+}
+
+
 void Grid() {
 
 	//header
 	system("cls");
-	cout << "\n\nTic Tac Toe \n\n";
-	cout << "Player 1 is X \t PLayer 2 is O\n" << endl;
-
+	
 	//drawing the Grid
 	cout << "     |     |     \n";
 	cout << "  " << grid[1] << "  |  " << grid[2] << "  |  " << grid[3] << endl;
@@ -71,40 +93,137 @@ void Grid() {
 }
 
 
-int main() {
-	int player =1;
-	int choice = 0;
-	int res = 0;
-	char mark;
+int singleplayer() {
+	cout << "Game mode: Single Player\n";
+	cout << "Your are playing as X\n";
+	cout << "Difficulty: Easy\n";
+	cout << "LOADING.";
+	Sleep(300);
+	cout << ".";
+	Sleep(300);
+	cout << ".";
+	Sleep(300);
+	cout << ".";
+	Sleep(300);
+	cout << ".";
+	Sleep(300);
+	cout << ".";
+	Sleep(3000);
 
+	int choice = 0;
+	int res = -1;
 	do {
 		Grid();
-		player = (player % 2) ? 1 : 2;	
-		cout << "player " << player << "`s turn, choose one of the availabe spots: ";
+		cout << "\nChoose a spot: ";
 		cin >> choice;
-		mark = (player == 1) ? 'X' : 'O';
-		if (grid[choice] != 'X' && grid[choice] != 'O') {
-			grid[choice] = mark;
+		if (grid[choice] != 'X' && grid[choice] != 'O' && choice <=9) {
+			grid[choice] = 'X';
+			res = status();
+			if (res == 1) {
+				Grid();
+				cout << "\n\aYOU Win!" << endl;
+				
+				return 0;
+			}
+			brainplay();
+			res = status();
+			if (res == 1) {
+				Grid();
+				cout << "\n\aComputer wins" << endl;
+				return 0;
+			}
+
 		}
 		else {
-			cout << "INVALID MOVE!";
-			player--;
+			cout << "INVALID MOVE! PRESS ENTER TO CONTINUE";
 			cin.ignore();
 			cin.get();
 		}
-		res = status();
-		player++;
-
+		
 	} while (res == -1);
 	Grid();
-	if (res == 1) {
-		cout << "\a Player " << --player << " Wins!" << endl;
-	}
-	else {
-		cout << "\aIt is a TIE" << endl;
-	}
 	cin.ignore();
 	cin.get();
+	return -1;
+}
+
+int versus(){
+	cout << "Game mode: Versus\n";
+	cout << "Player 1 is X \t PLayer 2 is O\n" << endl;
+	cout << "LOADING.";
+	Sleep(300);
+	cout << ".";
+	Sleep(300);
+	cout << ".";
+	Sleep(300);
+	cout << ".";
+	Sleep(300);
+	cout << ".";
+	Sleep(300);
+	cout << ".";
+	Sleep(3000);
+	int player = 1;
+	int choice = 0;
+	int res = 0;
+	char mark; 
+
+		do {
+			Grid();
+			player = (player % 2) ? 1 : 2;
+			cout << "\nplayer " << player << "'s turn, choose one of the availabe spots: ";
+			cin >> choice;
+			mark = (player == 1) ? 'X' : 'O';
+			if (grid[choice] != 'X' && grid[choice] != 'O') {
+				grid[choice] = mark;
+			}
+			else {
+				cout << "INVALID MOVE! PRESS ENTER";
+				player--;
+				cin.ignore();
+				cin.get();
+			}
+			res = status();
+			player++;
+
+		} while (res == -1);
+		Grid();
+		if (res == 1) {
+			cout << "\a Player " << --player << " Wins!" << endl;
+		}
+		else {
+			cout << "\aIt is a TIE" << endl;
+		}
+		cin.ignore();
+		cin.get();
+		return 0;
+
+}
+
+
+
+
+int main() {
+	cout << "\nTic Tac Toe \n\n";
+	char mode =' ';
+	while (mode != 'T') {
+	cout << "Choose your game mode:\n";
+	cout << "[A]single player \n[B] Versus\n";
+	cin >> mode;
+	if (mode == 'A' || mode == 'a') {
+		mode = 'T';
+		system("cls");
+		singleplayer();
+	}
+	else if (mode == 'B' || mode == 'b') {
+		system("cls");
+		mode = 'T';
+		versus();
+	}
+
+
+}
+	
+		
 	return 0;
 		
 }
